2a01a6c73b74c0efdcf2fbb7744b4682e2b66b9f
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-10-10 13:31:12 +0200

    updated quiz

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,96 +1,246 @@
-const questions = [
+const questions =   uestions = [    
     {
-        question: "Who is often referred to as the 'father of the computer' due to his development of the mechanical analytical engine, an early concept of a programmable computer?",
-        optionA: "Gordon Moore",
-        optionB: "Charles Babbage",
-        optionC: "William Shockley",
-        optionD: "Steve Jobs",
-        correctOption: "optionB"
+        "question": "Who is often referred to as the 'father of the computer' due to his development of the mechanical analytical engine, an early concept of a programmable computer?",
+        "optionA": "Gordon Moore",
+        "optionB": "Charles Babbage",
+        "optionC": "William Shockley",
+        "optionD": "Steve Jobs",
+        "correctOption": "optionB"
     },
-
     {
-        question: "What programming language, designed in 1957, was the first high-level programming language and primarily used for scientific and engineering calculations?",
-        optionA: "LISP",
-        optionB: "COBOL",
-        optionC: "Fortran",
-        optionD: "C",
-        correctOption: "optionC"
+        "question": "What programming language, designed in 1957, was the first high-level programming language and primarily used for scientific and engineering calculations?",
+        "optionA": "LISP",
+        "optionB": "COBOL",
+        "optionC": "Fortran",
+        "optionD": "C",
+        "correctOption": "optionC"
     },
-
     {
-        question: "In what year was JavaScript, the primary language for front-end web development, first introduced?",
-        optionA: "1984",
-        optionB: "1995",
-        optionC: "2004",
-        optionD: "2014",
-        correctOption: "optionB"
+        "question": "In what year was JavaScript, the primary language for front-end web development, first introduced?",
+        "optionA": "1984",
+        "optionB": "1995",
+        "optionC": "2004",
+        "optionD": "2014",
+        "correctOption": "optionB"
     },
-
     {
-        question: "Which co-founder of Intel is known for coining 'Moore's Law,' which predicts the regular doubling of computer power?",
-        optionA: "Charles Babbage",
-        optionB: "Larry Page",
-        optionC: "Gordon Moore",
-        optionD: "Mark Zuckerberg",
-        correctOption: "optionC"
+        "question": "Which co-founder of Intel is known for coining 'Moore's Law,' which predicts the regular doubling of computer power?",
+        "optionA": "Charles Babbage",
+        "optionB": "Larry Page",
+        "optionC": "Gordon Moore",
+        "optionD": "Mark Zuckerberg",
+        "correctOption": "optionC"
     },
-
     {
-        question: "What notable event in 1975 marked the beginning of Silicon Valley as a hub for technology and innovation?",
-        optionA: "Launch of the Macintosh",
-        optionB: "The Altair 8800 is released",
-        optionC: "Founding of Google",
-        optionD: "Launch of Windows 95",
-        correctOption: "optionB"
+        "question": "What notable event in 1975 marked the beginning of Silicon Valley as a hub for technology and innovation?",
+        "optionA": "Launch of the Macintosh",
+        "optionB": "The Altair 8800 is released",
+        "optionC": "Founding of Google",
+        "optionD": "Launch of Windows 95",
+        "correctOption": "optionB"
     },
-
     {
-        question: "Who are the co-founders of Google, the world's most widely used search engine?",
-        optionA: "Steve Jobs and Steve Wozniak",
-        optionB: "Larry Page and Sergey Brin",
-        optionC: "Bill Gates and Paul Allen",
-        optionD: "Chad Hurley, Steve Chen, and Jawed Karim",
-        correctOption: "optionB"
+        "question": "Who are the co-founders of Google, the world's most widely used search engine?",
+        "optionA": "Steve Jobs and Steve Wozniak",
+        "optionB": "Larry Page and Sergey Brin",
+        "optionC": "Bill Gates and Paul Allen",
+        "optionD": "Chad Hurley, Steve Chen, and Jawed Karim",
+        "correctOption": "optionB"
     },
-
     {
-        question: "What language, known for its simplicity and efficiency, is suitable for concurrent and networked systems and was developed by Google?",
-        optionA: "Ruby",
-        optionB: "Python",
-        optionC: "Go (Golang)",
-        optionD: "TypeScript",
-        correctOption: "optionC"
+        "question": "What language, known for its simplicity and efficiency, is suitable for concurrent and networked systems and was developed by Google?",
+        "optionA": "Ruby",
+        "optionB": "Python",
+        "optionC": "Go (Golang)",
+        "optionD": "TypeScript",
+        "correctOption": "optionC"
     },
-
     {
-        question: "What social media platform, founded in 2004, has changed the way people worldwide communicate and share information?",
-        optionA: "Twitter",
-        optionB: "Facebook",
-        optionC: "YouTube",
-        optionD: "WhatsApp",
-        correctOption: "optionB"
+        "question": "What social media platform, founded in 2004, has changed the way people worldwide communicate and share information?",
+        "optionA": "Twitter",
+        "optionB": "Facebook",
+        "optionC": "YouTube",
+        "optionD": "WhatsApp",
+        "correctOption": "optionB"
     },
-
     {
-        question: "Who is the creator of the Linux operating system, one of the most influential open-source projects in computer science?",
-        optionA: "Linus Torvalds",
-        optionB: "Bill Gates",
-        optionC: "Steve Jobs",
-        optionD: "Mark Zuckerberg",
-        correctOption: "optionA"
+        "question": "Who is the creator of the Linux operating system, one of the most influential open-source projects in computer science?",
+        "optionA": "Linus Torvalds",
+        "optionB": "Bill Gates",
+        "optionC": "Steve Jobs",
+        "optionD": "Mark Zuckerberg",
+        "correctOption": "optionA"
     },
-
     {
-        question: "In what year was the iPhone first presented to the public?",
-        optionA: "1992",
-        optionB: "2000",
-        optionC: "2007",
-        optionD: "2010",
-        correctOption: "optionC"
+        "question": "In what year was the iPhone first presented to the public?",
+        "optionA": "1992",
+        "optionB": "2000",
+        "optionC": "2007",
+        "optionD": "2010",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What year did COBOL, a programming language developed for business data processing, first appear?",
+        "optionA": "1950",
+        "optionB": "1955",
+        "optionC": "1959",
+        "optionD": "1965",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "Which programming language is often used in artificial intelligence and symbolic mathematics, and was created in 1958 by John McCarthy?",
+        "optionA": "LISP",
+        "optionB": "Fortran",
+        "optionC": "COBOL",
+        "optionD": "C",
+        "correctOption": "optionA"
+    },
+    {
+        "question": "Which programming language is known for its versatility, low-level capabilities, and is used for system programming and embedded systems?",
+        "optionA": "JavaScript",
+        "optionB": "Java",
+        "optionC": "C",
+        "optionD": "Python",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "In what year was the Macintosh computer launched, introducing a graphical user interface and a mouse?",
+        "optionA": "1984",
+        "optionB": "1992",
+        "optionC": "2000",
+        "optionD": "2006",
+        "correctOption": "optionA"
+    },
+    {
+        "question": "Which co-founder of Apple Inc. is known for his contributions to the development of the Apple I and Apple II computers?",
+        "optionA": "Steve Wozniak",
+        "optionB": "Steve Jobs",
+        "optionC": "Ronald Wayne",
+        "optionD": "Bill Gates",
+        "correctOption": "optionA"
+    },
+    {
+        "question": "What event marked the genesis of a tech empire that would shape the future of computing, involving Steve Jobs, Steve Wozniak, and Ronald Wayne?",
+        "optionA": "Founding of Microsoft",
+        "optionB": "Launch of the Macintosh",
+        "optionC": "Development of the Apple I",
+        "optionD": "Development of the first microchip",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What does the term 'Moore's Law' predict in the world of technology?",
+        "optionA": "Doubling of computer power every year",
+        "optionB": "Doubling of computer power every five years",
+        "optionC": "Doubling of computer power roughly every two years",
+        "optionD": "Stagnation of computer power",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "Which company was founded by Larry Page and Sergey Brin and is known for being the world's most widely used search engine?",
+        "optionA": "Yahoo",
+        "optionB": "Microsoft",
+        "optionC": "Google",
+        "optionD": "AltaVista",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What wireless technology, introduced in 1999, has become a common way to connect devices to the internet?",
+        "optionA": "Bluetooth",
+        "optionB": "Wi-Fi",
+        "optionC": "LTE",
+        "optionD": "NFC",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "In what year was YouTube founded, revolutionizing the way videos are shared and viewed online?",
+        "optionA": "1995",
+        "optionB": "2000",
+        "optionC": "2005",
+        "optionD": "2010",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What microcomputer kit, considered one of the pioneering personal computers, made its debut in 1975?",
+        "optionA": "Apple II",
+        "optionB": "Atari 2600",
+        "optionC": "Commodore 64",
+        "optionD": "Altair 8800",
+        "correctOption": "optionD"
+    },
+    {
+        "question": "Which language, known for its elegance and productivity, emphasizes simplicity and was created by Yukihiro Matsumoto in 1995?",
+        "optionA": "Ruby",
+        "optionB": "Python",
+        "optionC": "Scala",
+        "optionD": "Haskell",
+        "correctOption": "optionA"
+    },
+    {
+        "question": "What is the primary language for front-end web development, providing interactivity and dynamic content?",
+        "optionA": "Java",
+        "optionB": "JavaScript",
+        "optionC": "TypeScript",
+        "optionD": "HTML",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "Which language is designed for systems programming with a focus on safety, preventing memory-related bugs?",
+        "optionA": "Java",
+        "optionB": "Python",
+        "optionC": "Rust",
+        "optionD": "Scala",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What language combines object-oriented and functional programming features and runs on the JVM (Java Virtual Machine)?",
+        "optionA": "Ruby",
+        "optionB": "Scala",
+        "optionC": "Python",
+        "optionD": "JavaScript",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "Which language, introduced in 2012 by Microsoft, is a superset of JavaScript and adds static typing to improve code quality and maintainability?",
+        "optionA": "JavaScript",
+        "optionB": "TypeScript",
+        "optionC": "Java",
+        "optionD": "Python",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "What language is designed for iOS, macOS, watchOS, and tvOS app development, with a focus on safety and performance?",
+        "optionA": "Java",
+        "optionB": "Kotlin",
+        "optionC": "Swift",
+        "optionD": "Objective-C",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "Which statically-typed language runs on the Java Virtual Machine (JVM) and is known for its conciseness and safety features?",
+        "optionA": "Java",
+        "optionB": "Kotlin",
+        "optionC": "Ruby",
+        "optionD": "Scala",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "Who made significant contributions to the invention of the transistor and coined 'Moore's Law'?",
+        "optionA": "Charles Babbage",
+        "optionB": "William Shockley",
+        "optionC": "Gordon Moore",
+        "optionD": "John McCarthy",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "Who is known as the first programmer in history, thanks to her work on Charles Babbage's analytical engine and her notes that laid the foundation for modern programming languages?",
+        "optionA": "Ada Lovelace",
+        "optionB": "Grace Hopper",
+        "optionC": "Hedy Lamarr",
+        "optionD": "Rear Admiral Grace M. Hopper",
+        "correctOption": "optionA"
     }
-
-
 ]
+
 
 
 let shuffledQuestions = [] //empty array to hold shuffled selected questions
@@ -165,7 +315,7 @@ function checkForAnswer() {
             //set to delay question number till when next question loads
             setTimeout(() => {
                 questionNumber++
-            }, 500)
+            }, 1000)
         }
     })
 }
@@ -178,7 +328,7 @@ function handleNextQuestion() {
     unCheckRadioButtons()
     //delays next question displaying for a second
     setTimeout(() => {
-        if (indexNumber <= questions.length - 1) {
+        if (indexNumber <= 10) {
             NextQuestion(indexNumber)
         }
         else {
