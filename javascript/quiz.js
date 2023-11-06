7dfa43350077c27dc56a8a04f8374f171809f2ce
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-11-06 11:38:15 +0100

    quiz

---
+++ javascript/quiz.js
@@ -0,0 +1,430 @@
+const questions = [
+    {
+      "question": "Who is known as the 'father of the computer' due to his development of the mechanical analytical engine, an early concept of a programmable computer?",
+      "optionA": "Charles Babbage",
+      "optionB": "William Shockley",
+      "optionC": "Gordon Moore",
+      "optionD": "Linus Torvalds",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "Who is considered the first programmer in history due to her work on Babbage's analytical engine and her notes that laid the foundation for modern programming languages?",
+      "optionA": "Ada Lovelace",
+      "optionB": "Mark Zuckerberg",
+      "optionC": "Steve Jobs",
+      "optionD": "Larry Page",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "What year did Google, the world's most widely used search engine, get founded?",
+      "optionA": "1992",
+      "optionB": "1998",
+      "optionC": "1999",
+      "optionD": "2004",
+      "correctOption": "optionB"
+    },
+    {
+      "question": "Which of the following programming languages is known for its versatility and is used for system programming, embedded systems, and more?",
+      "optionA": "C",
+      "optionB": "Python",
+      "optionC": "Java",
+      "optionD": "JavaScript",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "What significant event marked the introduction of a graphical user interface (GUI) and the integration of a mouse for personal computers?",
+      "optionA": "The Altair 8800",
+      "optionB": "Windows 3.1",
+      "optionC": "Launch of the Macintosh",
+      "optionD": "Launch of Windows 95",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Who is known for coining 'Moore's Law,' which observes that the number of transistors on a microchip tends to double roughly every two years?",
+      "optionA": "Gordon Moore",
+      "optionB": "Bill Gates",
+      "optionC": "Mark Zuckerberg",
+      "optionD": "Larry Page",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "Which of these individuals is the co-founder of Facebook, a social media platform that has changed the way people worldwide communicate and share information?",
+      "optionA": "Linus Torvalds",
+      "optionB": "Kevin Mitnick",
+      "optionC": "Mark Zuckerberg",
+      "optionD": "Charles Babbage",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "In which year was Wi-Fi introduced, revolutionizing how we connect to the internet with wireless technology?",
+      "optionA": "1992",
+      "optionB": "1995",
+      "optionC": "2004",
+      "optionD": "1999",
+      "correctOption": "optionD"
+    },
+    {
+      "question": "Which of the following programming languages is known for its readability and ease of use, making it popular for web development, data science, and automation?",
+      "optionA": "Ruby",
+      "optionB": "Haskell",
+      "optionC": "Scala",
+      "optionD": "Python",
+      "correctOption": "optionD"
+    },
+    {
+      "question": "What is the primary language for front-end web development, providing interactivity and dynamic content?",
+      "optionA": "C",
+      "optionB": "Java",
+      "optionC": "TypeScript",
+      "optionD": "JavaScript",
+      "correctOption": "optionD"
+    },
+    {
+      "question": "Which co-founder of Apple Inc. played a pivotal role in the development of the first successful personal computer, the Apple I and the Apple II?",
+      "optionA": "Steve Jobs",
+      "optionB": "Steve Wozniak",
+      "optionC": "Ronald Wayne",
+      "optionD": "Mark Zuckerberg",
+      "correctOption": "optionB"
+    },
+    {
+      "question": "What was the widely acclaimed microcomputer kit that made its debut in 1975 and is frequently regarded as one of the pioneering personal computers?",
+      "optionA": "Commodore 64",
+      "optionB": "Apple II",
+      "optionC": "The Altair 8800",
+      "optionD": "Atari 2600",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Which language is known for emphasizing simplicity and productivity, with an elegant syntax?",
+      "optionA": "Fortran",
+      "optionB": "Ruby",
+      "optionC": "Scala",
+      "optionD": "COBOL",
+      "correctOption": "optionB"
+    },
+    {
+      "question": "What event marked the introduction of the TrueType font system, allowing Windows applications to use scalable fonts?",
+      "optionA": "Launch of the Macintosh",
+      "optionB": "Windows 95",
+      "optionC": "Windows 3.1",
+      "optionD": "Apple I release",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Which co-founder of Microsoft is known for significant influence on the personal computer industry?",
+      "optionA": "Bill Gates",
+      "optionB": "Steve Jobs",
+      "optionC": "Gordon Moore",
+      "optionD": "Mark Zuckerberg",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "In which year was the iPhone presented, marking a significant moment in the evolution of mobile technology?",
+      "optionA": "2004",
+      "optionB": "1998",
+      "optionC": "2007",
+      "optionD": "1999",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Which individual is known for creating the Linux operating system, one of the most influential open-source projects in the history of computer science?",
+      "optionA": "Charles Babbage",
+      "optionB": "Linus Torvalds",
+      "optionC": "Ada Lovelace",
+      "optionD": "Steve Jobs",
+      "correctOption": "optionB"
+    },
+    {
+      "question": "Which programming language is designed for scientific and engineering calculations and was the first high-level language?",
+      "optionA": "Fortran",
+      "optionB": "LISP",
+      "optionC": "COBOL",
+      "optionD": "C",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "In what year did the beginning of Silicon Valley, the globally renowned hub for technology and innovation, occur?",
+      "optionA": "1939",
+      "optionB": "1975",
+      "optionC": "1984",
+      "optionD": "2007",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "Which language is primarily used for building web applications, Android apps, and more, and is known for being platform-independent?",
+      "optionA": "Java",
+      "optionB": "TypeScript",
+      "optionC": "Python",
+      "optionD": "JavaScript",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "Who made significant contributions to the invention of the transistor and coined 'Moore's Law'?",
+      "optionA": "William Shockley",
+      "optionB": "Steve Jobs",
+      "optionC": "Larry Page",
+      "optionD": "Gordon Moore",
+      "correctOption": "optionD"
+    },
+    {
+      "question": "Which of the following programming languages is known for its purely functional nature and strong type inference?",
+      "optionA": "Haskell",
+      "optionB": "Python",
+      "optionC": "Ruby",
+      "optionD": "Scala",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "What technology, introduced in 1999, revolutionized how we connect to the internet using wireless signals?",
+      "optionA": "Dial-up internet",
+      "optionB": "Broadband internet",
+      "optionC": "Wi-Fi",
+      "optionD": "Fiber optic internet",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Which co-founder of YouTube revolutionized the way videos are shared and viewed online?",
+      "optionA": "Chad Hurley",
+      "optionB": "Kevin Mitnick",
+      "optionC": "Steve Chen",
+      "optionD": "Jawed Karim",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "What was the first social networking platform founded in 2004 by Mark Zuckerberg?",
+      "optionA": "Twitter",
+      "optionB": "Myspace",
+      "optionC": "Facebook",
+      "optionD": "YouTube",
+      "correctOption": "optionC"
+    },
+    {
+      "question": "Which co-founder of Apple Inc. was responsible for the development of the first successful personal computer, the Apple I and the Apple II?",
+      "optionA": "Steve Jobs",
+      "optionB": "Steve Wozniak",
+      "optionC": "Ronald Wayne",
+      "optionD": "Gordon Moore",
+      "correctOption": "optionB"
+    },
+    {
+      "question": "In what year was Windows 95 launched, accompanied by the song 'Start Me Up' by The Rolling Stones?",
+      "optionA": "1995",
+      "optionB": "2004",
+      "optionC": "2005",
+      "optionD": "1998",
+      "correctOption": "optionA"
+    },
+    {
+      "question": "Which individual is known for the creation of the transatlantic telegraph cable, significantly impacting global communication?",
+      "optionA": "Nikola Tesla",
+      "optionB": "Thomas Edison",
+      "optionC": "Samuel Morse",
+      "optionD": "Alexander Graham Bell",
+      "correctOption": "optionC"
+    },
+    {
+        "question": "What year was the Tesla Roadster Introduced, marking a significant step in the development of electric vehicles?",
+        "optionA": "2006",
+        "optionB": "2008",
+        "optionC": "2010",
+        "optionD": "2012",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "In which year was Instagram launched by Kevin Systrom and Mike Krieger?",
+        "optionA": "2008",
+        "optionB": "2010",
+        "optionC": "2011",
+        "optionD": "2014",
+        "correctOption": "optionB"
+    },
+    {
+        "question": "When was Siri, the virtual assistant developed by Apple, introduced with the release of the iPhone 4S?",
+        "optionA": "2009",
+        "optionB": "2010",
+        "optionC": "2011",
+        "optionD": "2012",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "What year did SpaceX, led by Elon Musk, become the first privately funded company to send a spacecraft to the International Space Station?",
+        "optionA": "2010",
+        "optionB": "2011",
+        "optionC": "2012",
+        "optionD": "2013",
+        "correctOption": "optionC"
+    },
+    {
+        "question": "In which year did Google unveil its self-driving car project, signaling a major advancement in autonomous vehicle technology?",
+        "optionA": "2012",
+        "optionB": "2013",
+        "optionC": "2014",
+        "optionD": "2015",
+        "correctOption": "optionC"
+    }
+]
+
+  
+
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() {
+    //function to shuffle and push 10 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= questions.length - 1) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0
+let wrongAttempt = 0
+let indexNumber = 0
+
+// function for displaying next question in the array
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 500)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 500);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    const totalQuestions = playerScore + wrongAttempt;
+    const percentage = Math.floor((playerScore / totalQuestions) * 100);
+    
+    if (percentage <= 30) {
+        remark = "Maybe read the website again";
+        remarkColor = "red";
+    }
+    else if (percentage > 30 && percentage <= 60) {
+        remark = "You are have clearly read the website";
+        remarkColor = "orange";
+    }
+    else if (percentage > 60) {
+        remark = "Excellent, You know this already.";
+        remarkColor = "green";
+    }
+    const playerGrade = percentage;
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
